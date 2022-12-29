e319079f294503f5a2ca1bbd9e9e1ab9b35510cc
117503445 <t117503445@gmail.com>
2022-12-30 03:22:15 +0800

    jaeger
    Signed-off-by: 117503445 <t117503445@gmail.com>

---
+++ jaeger/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+// newExporter returns a console exporter.
+func newExporter(w io.Writer) (trace.SpanExporter, error) {
+	return stdouttrace.New(
+		stdouttrace.WithWriter(w),
+		// Use human-readable output.
+		stdouttrace.WithPrettyPrint(),
+		// Do not print timestamps for the demo.
+		stdouttrace.WithoutTimestamps(),
+	)
+}
+
+// newResource returns a resource describing this application.
+func newResource() *resource.Resource {
+	r, _ := resource.Merge(
+		resource.Default(),
+		resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceNameKey.String("fib"),
+			semconv.ServiceVersionKey.String("v0.1.0"),
+			attribute.String("environment", "demo"),
+		),
+	)
+	return r
+}
+
+func main() {
+	fmt.Println("Hello World")
+}
